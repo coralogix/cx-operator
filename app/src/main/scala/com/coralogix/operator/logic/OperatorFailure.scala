227deb7a6d31fe17c29cb33571e0ec18ff21bd46
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2021-01-13 11:30:09 +0100

    ServiceAccount provisioning

--- app/src/main/scala/com/coralogix/operator/logic/OperatorFailure.scala
+++ app/src/main/scala/com/coralogix/operator/logic/OperatorFailure.scala
@@ -5,6 +5,7 @@ import com.coralogix.zio.k8s.operator.OperatorLogging.ConvertableToThrowable
 sealed trait CoralogixOperatorFailure
 case class GrpcFailure(status: io.grpc.Status) extends CoralogixOperatorFailure
 case class UndefinedGrpcField(name: String) extends CoralogixOperatorFailure
+case object ProvisioningFailed extends CoralogixOperatorFailure
 
 object CoralogixOperatorFailure {
   implicit val toThrowable: ConvertableToThrowable[CoralogixOperatorFailure] = {
@@ -12,5 +13,7 @@ object CoralogixOperatorFailure {
       status.asException()
     case UndefinedGrpcField(fieldName) =>
       new RuntimeException(s"Undefined field in gRPC data: $fieldName")
+    case ProvisioningFailed =>
+      new RuntimeException(s"Provisioning failed")
   }
 }
--- app/src/main/scala/com/coralogix/operator/logic/operators/coralogixlogger/CoralogixloggerOperator.scala
+++ app/src/main/scala/com/coralogix/operator/logic/operators/coralogixlogger/CoralogixloggerOperator.scala
@@ -1,21 +1,36 @@
 package com.coralogix.operator.logic.operators.coralogixlogger
 
-import com.coralogix.operator.logic.CoralogixOperatorFailure
+import com.coralogix.operator.logic.{ CoralogixOperatorFailure, ProvisioningFailed }
 import com.coralogix.operator.logic.aspects._
 import com.coralogix.operator.monitoring.OperatorMetrics
+import com.coralogix.zio.k8s.client.{ K8sFailure, NamespacedResource }
 import com.coralogix.zio.k8s.client.K8sFailure.syntax._
-import com.coralogix.zio.k8s.client.com.coralogix.loggers.coralogixloggers.v1.{Coralogixloggers, metadata}
-import com.coralogix.zio.k8s.client.com.coralogix.loggers.coralogixloggers.{v1 => coralogixloggers}
+import com.coralogix.zio.k8s.client.com.coralogix.loggers.coralogixloggers.v1.{
+  metadata,
+  replaceStatus,
+  Coralogixloggers
+}
+import com.coralogix.zio.k8s.client.com.coralogix.loggers.coralogixloggers.{
+  v1 => coralogixloggers
+}
 import com.coralogix.zio.k8s.client.com.coralogix.loggers.definitions.coralogixlogger.v1.Coralogixlogger
 import com.coralogix.zio.k8s.client.model._
 import com.coralogix.zio.k8s.client.serviceaccounts.v1.ServiceAccounts
-import com.coralogix.zio.k8s.client.serviceaccounts.{v1 => serviceaccounts}
-import com.coralogix.zio.k8s.operator.Operator.{EventProcessor, OperatorContext}
+import com.coralogix.zio.k8s.client.serviceaccounts.{ v1 => serviceaccounts }
+import com.coralogix.zio.k8s.model.core.v1.ServiceAccount
+import com.coralogix.zio.k8s.operator.Operator.{ EventProcessor, OperatorContext }
+import com.coralogix.zio.k8s.operator.OperatorLogging._
 import com.coralogix.zio.k8s.operator.aspects.logEvents
-import com.coralogix.zio.k8s.operator.{KubernetesFailure, Operator, OperatorFailure}
-import zio.ZIO
+import com.coralogix.zio.k8s.operator.{
+  KubernetesFailure,
+  Operator,
+  OperatorError,
+  OperatorFailure,
+  OperatorLogging
+}
+import zio.{ Cause, Has, ZIO }
 import zio.clock.Clock
-import zio.logging.{Logging, log}
+import zio.logging.{ log, Logging }
 
 object CoralogixloggerOperator {
   private def eventProcessor(): EventProcessor[
@@ -39,7 +54,9 @@ object CoralogixloggerOperator {
   private def setupLogger(
     ctx: OperatorContext,
     resource: Coralogixlogger
-  ): ZIO[Logging with Coralogixloggers with ServiceAccounts, OperatorFailure[CoralogixOperatorFailure], Unit] =
+  ): ZIO[Logging with Coralogixloggers with ServiceAccounts, OperatorFailure[
+    CoralogixOperatorFailure
+  ], Unit] =
     skipIfAlredyRunning(resource) {
       for {
         name <- resource.getName.mapError(KubernetesFailure.apply)
@@ -53,6 +70,9 @@ object CoralogixloggerOperator {
           )
         _ <- setupServiceAccount(ctx, name, uid, resource)
       } yield ()
+    }.catchSome {
+      case OperatorError(ProvisioningFailed) =>
+        log.info(s"Provision failed")
     }
 
   private def skipIfAlredyRunning[R <: Logging](resource: Coralogixlogger)(
@@ -87,12 +107,27 @@ object CoralogixloggerOperator {
       .mapError(KubernetesFailure.apply)
   }
 
+  private def provisioningFailed(
+    resource: Coralogixlogger,
+    phase: String,
+    reason: String,
+    k8sReason: K8sFailure
+  ): ZIO[Coralogixloggers with Logging, OperatorFailure[CoralogixOperatorFailure], Nothing] =
+    logFailure(
+      reason,
+      Cause.fail[OperatorFailure[CoralogixOperatorFailure]](KubernetesFailure(k8sReason))
+    ) *>
+      updateState(resource, "FAILED", phase, reason) *>
+      ZIO.fail(OperatorError(ProvisioningFailed))
+
   private def setupServiceAccount(
     ctx: OperatorContext,
     name: String,
     uid: String,
     resource: Coralogixlogger
-  ): ZIO[ServiceAccounts, OperatorFailure[CoralogixOperatorFailure], Unit] = {
+  ): ZIO[Coralogixloggers with Logging with Has[
+    NamespacedResource[ServiceAccount]
+  ], OperatorFailure[CoralogixOperatorFailure], Unit] = {
     val serviceAccount =
       Model.attachOwner(name, uid, ctx.resourceType, Model.serviceAccount(name, resource))
 
@@ -107,9 +142,52 @@ object CoralogixloggerOperator {
                        .ifFound
                        .either
       _ <- queryResult match {
-             case Left(failure) => ZIO.unit // TODO
-             case Right(None)   => ZIO.unit // TODO
-             case Right(_)      => ZIO.unit // TODO
+             case Left(failure) =>
+               provisioningFailed(
+                 resource,
+                 phase = "ServiceAccount",
+                 reason = "Provisioning of ServiceAccount failed.",
+                 failure
+               )
+             case Right(None) =>
+               for {
+                 _ <- updateState(
+                        resource,
+                        "PROVISIONING",
+                        "ServiceAccount",
+                        "Provisioning of ServiceAccount..."
+                      )
+                 _ <- log.info(s"Creating a new ServiceAccount with name $serviceAccountName")
+                 _ <- serviceaccounts
+                        .create(serviceAccount, namespace)
+                        .catchAll { failure =>
+                          provisioningFailed(
+                            resource,
+                            phase = "ServiceAccount",
+                            reason = "Provisioning of ServiceAccount failed.",
+                            failure
+                          )
+                        }
+                 _ <- replaceStatus(
+                        resource,
+                        resource.status
+                          .getOrElse(Coralogixlogger.Status())
+                          .copy(
+                            serviceAccount = Some(serviceAccountName),
+                            reason = Some("Provisioning of ServiceAccount successful.")
+                          ),
+                        namespace
+                      ).mapError(KubernetesFailure.apply)
+               } yield ()
+             case Right(_) =>
+               log.info(s"Skip: ServiceAccount already exists") *>
+                 replaceStatus(
+                   resource,
+                   resource.status
+                     .getOrElse(Coralogixlogger.Status())
+                     .copy(serviceAccount = Some(serviceAccountName)),
+                   namespace
+                 ).mapError(KubernetesFailure.apply)
            }
     } yield ()
   }
